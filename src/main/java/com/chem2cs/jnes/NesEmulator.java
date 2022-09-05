0e2ef6436eacf789bf7e30786a8c5199d044db82
zhaohan <zhaohan@kuaishou.com>
2022-09-05 21:31:22 +0800

    first commit

---
+++ src/main/java/com/chem2cs/jnes/NesEmulator.java
@@ -0,0 +1,20 @@
+package com.chem2cs.jnes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhaohan
+ * Created on 2022-09-05
+ */
+
+public class NesEmulator {
+
+    private static final Logger logger = LoggerFactory.getLogger(NesEmulator.class);
+
+    public static void main(String[] args) {
+
+        logger.info("log {}",123);
+        System.out.println(123);
+    }
+}
